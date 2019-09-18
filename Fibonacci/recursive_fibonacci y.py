05153a8d7b6a222f2e898e8db8caef18e50a636b
Mazhar004 <princemazhar.mp@gmail.com>
2019-09-18 15:36:03 +0600

    Add document in fibonacci program

--- Fibonacci/recursive_fibonacci y.py
+++ Fibonacci/recursive_fibonacci y.py
@@ -1,10 +1,26 @@
-def fibonacci_recursion(count,previous=0,current=1):
-    '''Here Previous and Current parameters are optional. So user did not need to input this value'''
-    if count:
-        print(previous,end=' ')
-        fibonacci_recursion(count-1,current,previous+current)
+def fibonacci_recursion(count, previous=0, current=1):
+    '''
+        Here Previous and Current parameters are optional. So user did not need to input this value
 
-        
-        
-n=int(input('Enter Number of Element for Fibonacci Series'))
-fibonacci_recursion(n)
+        count = Number of value you want to print
+
+        Example 1 : 
+            count=3
+            output= 0 1 1
+
+        -----------------------
+
+        Example 2 :
+            count=6
+            output= 0 1 1 2 3 5
+
+    '''
+    if count:
+        print(previous, end=' ')
+        fibonacci_recursion(count-1, current, previous+current)
+
+
+if __name__ == "__main__":
+
+    n = int(input('Enter Number of Element for Fibonacci Series = '))
+    fibonacci_recursion(n)
