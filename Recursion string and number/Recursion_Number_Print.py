e106b1534de857674cefa01556933dca5b67bdd8
Mazhar004 <princemazhar.mp@gmail.com>
2019-09-18 16:08:49 +0600

    Add document in recursive numer & reverse string print program

--- Recursion string and number/Recursion_Number_Print.py
+++
@@ -1,7 +0,0 @@
-def recursion_number(a):
-    '''Print each digit in a number seperately using recursion'''
-    if a>9:
-        reverse_number(a//10)
-    print(a%10,end=" ")
-
-reverse_number(1234)
--- Recursion string and number/Reverse_String_Recursion.py
+++
@@ -1,7 +0,0 @@
-def reverse_string(a,i=0):
-    ''' String Reverse using recursion '''
-    if i < len(a)-1:
-        reverse_string(a,i+1)
-    print(a[i],end="")
-    
-reverse_string('Hello World')
