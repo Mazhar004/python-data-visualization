e663f093b91c099c30502bf84185a45c695d64fb
Mazhar004 <princemazhar.mp@gmail.com>
2019-09-18 16:08:49 +0600

    Add document in recursive numer & reverse string print program

---
+++ Recursion Number Print/Recursion_Number_Print.py
@@ -0,0 +1,15 @@
+def recursion_number(n):
+    '''
+    n = Number
+
+    Print each digit of "n" seperately using recursion
+    '''
+    if n > 9:
+        recursion_number(n//10)
+    print(n % 10, end=" ")
+
+
+if __name__ == "__main__":
+
+    n = int(input('Enter the number = '))
+    recursion_number(n)
