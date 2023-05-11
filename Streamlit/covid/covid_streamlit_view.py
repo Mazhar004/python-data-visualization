4da0df53c9886010b9fb4cb12fa32b3dfc62c05f
mazhar004 <princemazhar.mp@gmail.com>
2023-05-11 21:54:59 +0700

    Refactor streamlit view - Use col to side by side vertical view - Improve subheader text

--- Streamlit/covid/covid_streamlit_view.py
+++ Streamlit/covid/covid_streamlit_view.py
@@ -7,27 +7,24 @@ from covid_fig import covid_analyze_lines, covid_analyze_bars
 
 
 def covid_lines_stats(df):
-    ### Country Wise ###
+    # Country Wise
     country_list = df['Country'].unique().tolist()
     d_type_list = df['Dataset'].unique().tolist()
 
     min_date = min(df.columns[2:]).date()
     max_date = max(df.columns[2:]).date()
 
-    row1_spacer1, row1, row1_spacer2 = st.columns((1, 31, 1))
-    with row1:
-        st.subheader('Country Wise Analysis')
+    st.subheader('COVID-19 Line Chart by Country')
 
-    row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3 = st.columns(
-        (1, 10, 1, 20, 1))
-    with row2_1:
+    col1, col2 = st.columns([1, 3])
+    with col1:
         countries = st.multiselect(
-            "Which country do you want to analyze?", country_list, key='Country Name')
+            "Select countries to analyze", country_list, key='Country Name')
         d_types = st.multiselect(
-            "For Which type of data?", d_type_list, key='Data Type')
-        date_range = st.slider("Select a range of dates", min_value=min_date,
+            "Select data types", d_type_list, key='Data Type')
+        date_range = st.slider("Select a date range", min_value=min_date,
                                max_value=max_date, value=(min_date, max_date), key='Line Date')
-    with row2_2:
+    with col2:
         temp_df = country_filter(df, countries)
         temp_df = dtype_filter(temp_df, d_types)
         temp_df = date_filter(temp_df, date_range[0], date_range[1])
@@ -37,26 +34,23 @@ def covid_lines_stats(df):
 
 
 def covid_bars_stats(df):
-    ### Top Country ###
+    # Top Country
     d_type_list = df['Dataset'].unique().tolist()
 
     min_date = min(df.columns[2:]).date()
     max_date = max(df.columns[2:]).date()
 
-    row1_spacer1, row1, row1_spacer2 = st.columns((1, 31, 1))
-    with row1:
-        st.subheader('Top Country')
+    st.subheader('Top Countries Affected by COVID-19')
 
-    row2_spacer1, row2_1, row2_spacer2, row2_2, row2_spacer3 = st.columns(
-        (1, 10, 1, 20, 1))
-    with row2_1:
+    col1, col2 = st.columns([1, 3])
+    with col1:
         top_k = st.selectbox(
-            "Top K countries", [10, 15, 20, 25, 30], key='Top K')
+            "Select number of countries", [10, 15, 20, 25, 30], key='Top K')
         d_types = st.selectbox(
-            "For Which type of data?", d_type_list, key='Data Type Single')
-        date_range = st.slider("Select a range of dates", min_value=min_date,
+            "Select data type", d_type_list, key='Data Type Single')
+        date_range = st.slider("Select a date range", min_value=min_date,
                                max_value=max_date, value=(min_date, max_date), key='Bar Date')
-    with row2_2:
+    with col2:
         temp_df = dtype_filter(df, [d_types])
         temp_df = date_filter(temp_df, date_range[0], date_range[1])
         temp_df = build_bar_data(temp_df)
