7dc0c14eeadd6cf53117bbe2b0c8cb99afb9460d
mazhar004 <princemazhar.mp@gmail.com>
2023-05-17 23:54:51 +0700

    educe utils function - Date filter using index(column)

--- Streamlit/covid/utils.py
+++ Streamlit/covid/utils.py
@@ -1,47 +1,25 @@
-import datetime
 import pandas as pd
 
 
-def country_filter(df, countries):
-    temp_df = df[df['Country'].isin(countries)]
-    return temp_df
+def date_filter(df, st_index, lt_index):
+    return df.iloc[:, st_index:lt_index+1]
 
 
-def dtype_filter(df, d_types):
-    temp_df = df[df['Dataset'].isin(d_types)]
-    return temp_df
-
-
-def date_filter(df, start_date, end_date):
-    start_date = datetime.datetime.combine(
-        start_date, datetime.datetime.min.time())
-    end_date = datetime.datetime.combine(
-        end_date, datetime.datetime.min.time())
-
-    temp_df = df.copy()
-    start_loc = temp_df.columns.get_loc(start_date)
-    temp_df = temp_df.iloc[:, :2].join(temp_df.iloc[:, start_loc:])
-
-    end_loc = temp_df.columns.get_loc(end_date)
-    temp_df = temp_df.iloc[:, :end_loc]
-
-    return temp_df
-
-
-def build_bar_data(df):
+def build_bar_data(df, d_types):
     transform_df = df.mask(df == 0).ffill(axis=1)
     data = []
-    for _, row in transform_df.iterrows():
+    for country, row in transform_df.iterrows():
         val = row.iloc[-1]
         try:
             val = float(val)
         except:
             val = 0
-        data.append([row.Dataset, row.Country, val])
+        data.append([country, val])
 
-    columns = ['Dataset', 'Country', 'Value']
+    columns = ['Country', 'Value']
     f_df = pd.DataFrame(data, columns=columns).sort_values(by='Value',
                                                            ascending=False)
+    f_df['Dataset'] = d_types
 
     return f_df
 
