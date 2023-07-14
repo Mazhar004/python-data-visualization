f6b1a26bc6301554be25e6e69e9f4dde65db7b46
mazhar004 <princemazhar.mp@gmail.com>
2023-07-14 18:34:15 +0600

    Generate specific person wise visualization - Call frequency - Total/Average call time - Hourly timeseries for call frequency

--- Streamlit/Call Log Analysis/utils/figview.py
+++ Streamlit/Call Log Analysis/utils/figview.py
@@ -58,3 +58,35 @@ def call_time_fig(df, name='My'):
     return fig
 
 
+def person_wise_stats(df, name):
+    index_dict = {'call_time': {'Incoming': 'Incoming CallTime (Minute)',
+                                'Outgoing': 'Outgoing CallTime (Minute)'},
+                  'avg_call_time': {'Incoming': 'Incoming Avg CallTime (Seconds)',
+                                    'Outgoing': 'Outgoing Avg CallTime (Seconds)'}
+                  }
+    field_rename = {'Unanswered': f'Rejected by {name}',
+                    'Call Rejected': 'Rejected by Me'}
+    person_df = df[df.Name == name]
+
+    direction_df = individual_count(person_df, 'Direction')
+    status_df = individual_count(person_df, 'Status')
+
+    call_time = individual_group(person_df,
+                                 cal_total_min,
+                                 index_dict['call_time'])
+    avg_call_time = individual_group(person_df,
+                                     average_total_sec,
+                                     index_dict['avg_call_time'])
+
+    temp_df = pd.concat([direction_df, status_df, call_time,
+                        avg_call_time]).rename(index=field_rename)
+
+    fig1 = px.bar(temp_df,
+                  x=temp_df.index,
+                  y='Frequency',
+                  title=f'{name} Call Stats',
+                  color=temp_df.index)
+
+    fig2 = call_time_fig(person_df, name)
+
+    return fig1, fig2
